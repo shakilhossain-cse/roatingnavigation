72822ed748a60c4f7fbcd29ad4d37c60d1ac65bc
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-09-19 12:38:12 +0600

    added class by javascript

---
+++ app.js
@@ -0,0 +1,16 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
+
+
+open.addEventListener('click',()=>{
+    container.classList.add('show-nav')
+    open.style.display='none';
+     close.style.display='block';
+
+});
+close.addEventListener('click',()=>{
+    container.classList.remove('show-nav')
+    close.style.display='none';
+    open.style.display='block';
+});
